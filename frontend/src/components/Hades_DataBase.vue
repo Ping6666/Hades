23676b26c5ee2500dbd097f31474a539a1be832f
Ping6666 <Ping6666@users.noreply.github.com>
2023-01-13 20:48:53 +0800

    frontend

---
+++ frontend/src/components/Hades_DataBase.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>{{ msg }}</p>
+  <button @click="fetcher">fetch backend/api/auth</button>
+  <p>{{ rt }}</p>
+</template>
+
+<script>
+export default {
+  name: 'Hades_DataBase',
+  data() {
+    return {
+      rt: '',
+    }
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+    async fetcher() {
+      // const res = await fetch('http://localhost:3000/api', {
+      const res = await fetch('http://localhost:3000/users', {
+        method: 'GET',
+        // method: 'POST',
+      });
+
+      console.log(res);
+      var _res = await res.json();
+      this.rt = _res.message;
+    }
+  }
+}
+</script>
