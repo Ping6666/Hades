088036fe3f17170eb4f4c6a742e765b5fb0c0e6c
Ping6666 <Ping6666@users.noreply.github.com>
2023-01-17 21:44:36 +0800

    CRUD api server side

--- backend/routes/db.js
+++ backend/routes/db.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var object_id = require('mongodb').ObjectId;
 var db_server = require('../database/server')
+
+var get_object_id = (c_id) => {
+    return new object_id(c_id);
+};
 
 router.get('/', async function (req, res, next) {
     res.json({ 'message': 'this is db' });
@@ -9,13 +14,38 @@ router.get('/', async function (req, res, next) {
 
 router.post('/create', async function (req, res, next) {
     console.log(req.body);
-    var db_rt_message = await db_server.db_create(req.query.db, req.query.coll, req.body);
-    res.json({ 'message': db_rt_message });
+    db_server.db_create(req.query.db, req.query.coll, req.body);
 });
 
 router.post('/read', async function (req, res, next) {
+    console.log(req.body);
+
+    if (req.body._id) {
+        req.body._id = get_object_id(req.body._id[0]);
+    }
+
     var db_rt_message = await db_server.db_read(req.query.db, req.query.coll, req.body);
     res.json({ 'message': db_rt_message });
+});
+
+router.post('/update', async function (req, res, next) {
+    console.log(req.body);
+
+    const c_id = get_object_id(req.body._id[0]);
+    const c_item = { $set: req.body.item };
+    db_server.db_update(req.query.db, req.query.coll, c_id, c_item);
+});
+
+router.post('/delete', async function (req, res, next) {
+    console.log(req.body);
+
+    const c_ids = req.body._id;
+
+    c_ids.forEach(async (_id) => {
+        const c_id = get_object_id(_id);
+
+        db_server.db_delete(req.query.db, req.query.coll, c_id);
+    });
 });
 
 module.exports = router;
